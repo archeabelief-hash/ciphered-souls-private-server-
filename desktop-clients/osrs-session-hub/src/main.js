6bdee10227b9bd333bdd3fd4ecafd2047261aabe
archeabelief-hash <archeabelief@gmail.com>
2026-04-30 13:18:44 -0700

    Add detection and tiling system

--- desktop-clients/osrs-session-hub/src/main.js
+++ desktop-clients/osrs-session-hub/src/main.js
@@ -5,8 +5,8 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    width: 1400,
+    height: 900,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
@@ -20,5 +20,18 @@ app.whenReady().then(createWindow);
 
 ipcMain.handle('launch-client', async (event, path) => {
   exec(`"${path}"`);
+  return true;
+});
+
+ipcMain.handle('detect-windows', async () => {
+  return new Promise((resolve) => {
+    exec(`powershell "Get-Process | Where-Object { $_.MainWindowTitle -like '*RuneLite*' -or $_.MainWindowTitle -like '*Old School RuneScape*' } | Select-Object Id,MainWindowTitle"`, (err, stdout) => {
+      resolve(stdout);
+    });
+  });
+});
+
+ipcMain.handle('tile-windows', async () => {
+  exec(`powershell -Command \"$sig = '[DllImport(\"user32.dll\")]public static extern bool MoveWindow(IntPtr hWnd,int X,int Y,int nWidth,int nHeight,bool bRepaint);'; add-type -MemberDefinition $sig -Name Win32 -Namespace API; $procs = Get-Process | Where-Object {$_.MainWindowTitle -like '*RuneLite*'}; $i=0; foreach ($p in $procs){ $x = ($i%2)*800; $y = [math]::Floor($i/2)*450; [API.Win32]::MoveWindow($p.MainWindowHandle,$x,$y,800,450,$true); $i++ }\"`);
   return true;
 });
