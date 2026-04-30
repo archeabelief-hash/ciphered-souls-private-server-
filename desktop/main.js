3a1045f4ac375d56ba9a0bb8f8e8ac33f847b361
archeabelief-hash <archeabelief@gmail.com>
2026-04-30 11:17:17 -0700

    Switch client to real RSPS launcher

--- desktop/main.js
+++ desktop/main.js
@@ -1,31 +1,10 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const { startServer } = require(path.join(__dirname, '..', 'src', 'server'));
+const { app } = require('electron');
+const { launchRSPS } = require('./rspsLauncher');
 
-let mainWindow;
-let serverInstance;
-
-async function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 800,
-    autoHideMenuBar: true,
-    webPreferences: {
-      contextIsolation: true
-    }
-  });
-
-  try {
-    serverInstance = await startServer(8787, '127.0.0.1');
-    await mainWindow.loadURL('http://127.0.0.1:8787/index.html');
-  } catch (error) {
-    console.error('Failed to start server:', error);
-  }
-}
-
-app.whenReady().then(createWindow);
+app.whenReady().then(async () => {
+  await launchRSPS();
+});
 
 app.on('window-all-closed', () => {
-  if (serverInstance) serverInstance.close();
   if (process.platform !== 'darwin') app.quit();
 });
